ae2b9567936dae211c79193b22dc32e14aa56bdd
Daniela7710 <danieladiazgomez97@gmail.com>
2020-01-28 15:02:21 -0500

    feature/opacity

--- src/components/CircleImage.js
+++ src/components/CircleImage.js
@@ -6,7 +6,7 @@ const CircleImage = (props) => {
     const { value: { id, name, image }, type } = props;
     return (
         <Link to={type === "artist" ? `/artists/${id}/albums` : `/albums/${id}/songs`} >
-            <div className="circle">
+            <div className={type === "artist" ?"circle circle_opacity":"circle"}>
                 <img className="circle__image" src={image} alt={name} />
                 <span className={`circle__name ${type === "artist" ? "circle__name--centered" : "circle__name--bottom"}`}>
                     {name}
