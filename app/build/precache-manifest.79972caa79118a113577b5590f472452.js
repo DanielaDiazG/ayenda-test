decc1902a0484444eb503212e3b17d021844db97
Daniela7710 <danieladiazgomez97@gmail.com>
2020-01-28 19:30:51 -0500

    feature/folders

---
+++ app/build/precache-manifest.79972caa79118a113577b5590f472452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c644b933d9161d55d59d7dcd3e4b89ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e811c73b3819f6b7e4",
+    "url": "/static/css/main.aff3457d.chunk.css"
+  },
+  {
+    "revision": "dd028a149867e1652ac8",
+    "url": "/static/js/2.feb9cd40.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.feb9cd40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22e811c73b3819f6b7e4",
+    "url": "/static/js/main.f5b32586.chunk.js"
+  },
+  {
+    "revision": "67c0d0fddbb2cb1b8d06",
+    "url": "/static/js/runtime-main.3e6cfa2e.js"
+  }
+]);
